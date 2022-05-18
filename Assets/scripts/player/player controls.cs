a18138aeaa846320423399b5c544c2c4b36e4df8
Thywar <graywolf07882005@hotmail.com>
2022-05-18 18:05:52 -0400

    update animation

--- Assets/scripts/player/player controls.cs
+++ Assets/scripts/player/player controls.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontroller : MonoBehaviour
+public class playercontrols : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
