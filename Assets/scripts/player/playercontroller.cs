b3614e74e5c80fd68c969d602e283e5283d89c07
Thywar <graywolf07882005@hotmail.com>
2022-05-19 11:06:16 -0400

    update 4

--- Assets/scripts/player/playercontroller.cs
+++ Assets/scripts/player/playercontroller.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontrols : MonoBehaviour
+public class playercontroller : MonoBehaviour
 {
     public Rigidbody2D rg;
     public Animator anim;
     public SpriteRenderer sr;
-
     // Start is called before the first frame update
     void Start()
     {
