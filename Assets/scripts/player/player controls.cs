0e788d250318dada6dcd9418f10997fa31ea9f0a
Thywar <graywolf07882005@hotmail.com>
2022-05-19 09:09:07 -0400

    update 3

--- Assets/scripts/player/player controls.cs
+++ Assets/scripts/player/player controls.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class playercontrols : MonoBehaviour
 {
+    public Rigidbody2D rg;
+    public Animator anim;
+    public SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
